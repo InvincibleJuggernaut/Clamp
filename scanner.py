75e5c0d1691c457bb0d01ee9eb3ab0ca53cdf914
Manikandan Nair <mani4pmn@gmail.com>
2021-05-29 07:08:51 +0000

    Minor changes

--- scanner.py
+++ scanner.py
@@ -39,20 +39,35 @@ def get_stats(resp):
 
     print()
     print("Name : ")
-    for x in name:
+    for x in name[0:3]:
         print(" "+x)
     print("\nSize : "+str(size)+" bytes")
     print("\nTimes submitted : ", times_submitted)
     print("\nLast modification date : ", last_modification_date)
     print()
 
+    legitimate_count = 0
+    engine_count = 0
+
     for x in resp['data']['attributes']['last_analysis_results']:
         engine_name = x
+        engine_count += 1
         category = resp['data']['attributes']['last_analysis_results'][x]['category']
         result = resp['data']['attributes']['last_analysis_results'][x]['result']
-        if(category!='undetected' and category!='type-unsupported' and category!='timeout' and category!='failure'):
+        if(category=='malicious'):
             print(str(engine_name).ljust(25) +" - "+ Fore.RED + str(category).ljust(20) + Style.RESET_ALL+ " - "+ str(result))
         elif(category =='undetected'):
+            legitimate_count += 1
             print(str(engine_name).ljust(25) +" - "+ Fore.GREEN + str(category).ljust(20) + Style.RESET_ALL+ " - "+ str(result))
         else:
             print(str(engine_name).ljust(25) +" - "+ str(category).ljust(20) +" - "+ str(result))
+    
+    print("\n")
+    if(legitimate_count/engine_count >= 0.7):
+        print(str(legitimate_count)+" out of "+str(engine_count)+" engines detected this file as legitimate")
+        print("File appears to be legitimate")
+    elif(legitimate_count/engine_count >=0.5 and legitimate_count/engine_count <0.7):
+        print(str(legitimate_count)+" out of "+str(engine_count)+" engines detected this file as legitimate")
+    else:
+        print("Only "+str(legitimate_count)+" out of "+str(engine_count)+" engines detected this file as legitimate")
+        print("File appears to be malware")

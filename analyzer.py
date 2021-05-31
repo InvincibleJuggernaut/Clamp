42b52c8b795c5da6fbd7a6c2322aab57fb0e8f27
Manikandan Nair <mani4pmn@gmail.com>
2021-05-31 05:43:52 +0000

    Minor change in interface

--- analyzer.py
+++ analyzer.py
@@ -1,9 +1,17 @@
+import os
 import pickle
 import numpy as np
+
+from art import art
 from pe import extract
 from scanner import find_hash, search_vt, get_stats
 
-file = 'arduino.exe'
+os.system('clear')
+
+print(art)
+
+file = input('Enter file name with appropriate directory : ')
+print()
 
 hash = find_hash(file)
 resp = search_vt(hash)
